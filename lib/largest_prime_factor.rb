5a60d4dccc45876e17a064b7ef987de5819ac06a
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-12 19:16:26 +0000

    Procedure & OOP solutions to Project Euler's LPF

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,46 @@
 # Enter your procedural solution here!
+require 'pry'
+
+prime_nums = [3]
+
+def largest_prime_factor(input, prime_nums=[3])
+  return 2 if input == 2
+  
+  while input % 2 == 0
+    input = input/2
+  end
+
+  while input % 3 == 0
+    input = input/3
+  end
+
+  while true
+    #binding.pry
+    next_prime = next_prime_number(prime_nums)
+    if input > next_prime
+      while input % next_prime == 0 && input != next_prime
+        input =  input / next_prime
+      end
+    else
+      return input
+    end
+  end
+end
+
+def next_prime_number(prime_nums)
+  current_num = prime_nums[-1] + 2
+
+  while true
+    if is_prime?(prime_nums, current_num)
+      prime_nums << current_num #Take it somewhere else goddamnit! A method is supposed to do ONE thing
+      return current_num
+    else
+      current_num += 2
+    end
+  end
+end
+
+def is_prime?(prime_nums, number)
+  return true if prime_nums.all? {|pm| number % pm > 0 }
+  false
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,53 @@
 # Enter your object-oriented solution here!
+
+# Enter your procedural solution here!
+require 'pry'
+
+class LargestPrimeFactor
+  def initialize(number)
+    @number = number
+    @prime_nums=[3]
+  end
+    
+  def number
+    return 2 if @number == 2
+
+    while @number % 2 == 0
+      @number = @number/2
+    end
+
+    while @number % 3 == 0
+      @number = @number/3
+    end
+
+    while true
+      #binding.pry
+      next_prime = next_prime_number
+      if @number > next_prime
+        while @number % next_prime == 0 && @number != next_prime
+          @number =  @number / next_prime
+        end
+      else
+        return @number
+      end
+    end
+  end
+
+  def next_prime_number
+    current_num = @prime_nums[-1] + 2
+
+    while true
+      if is_prime?(current_num)
+        @prime_nums << current_num #Take it somewhere else goddamnit! A method is supposed to do ONE thing
+        return current_num
+      else
+        current_num += 2
+      end
+    end
+  end
+
+  def is_prime?(number)
+    return true if @prime_nums.all? {|pm| number % pm > 0 }
+    false
+  end
+end
